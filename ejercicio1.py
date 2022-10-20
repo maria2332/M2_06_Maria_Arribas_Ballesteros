ae81c37fa7c6ab61d6e7d57fddff630690ddc079
maria2332 <maria.ballesteros2004@gmail.com>
2022-10-20 09:10:31 +0200

    ejercicio1

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,8 +1,9 @@
 import math
 
 def area_circulo():
- radio = int(input("Introduce el valor del radio: "))
- area = math.pi * (radio^2)
+ radio = input("Introduce el valor del radio: ")
+ radio=int(radio)
+ area = math.pi * (radio**2)
  return print(area)
 
 area_circulo()
